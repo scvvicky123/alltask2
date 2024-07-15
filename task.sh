fe3cc4b59392fbfa19942537e40dacc02dca9f8a
origin <scvvicky@gmail.com>
2024-07-15 13:46:27 +0000

    Modified changes in task.sh and vowel1.sh

--- task.sh
+++ task.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-
-#Credentials
-
-read -p "Enter your username - " username
-read -sp "Enter your password - " password
-echo
-username="admin"
-password="Administrator123"
-echo
-echo "Welcome, $username!"
-
-
+# Prompt the user for a username
+read -p "Enter your username: " username
+read -sp "Enter your password: " password
+echo  
+if [ "$username" == "admin" ] && [ "$password" == "Administrator123" ]; then
+    echo "Login successful!"
+else
+    echo "Login failed. Incorrect username or password."
+fi
+if [ "$username" == "Developer" ] && [ "$password" == "JavaPython@123" ]; then
+    echo "Login successful!"
+else
+    echo "Login failed. Incorrect username or password."
+fi
--- vowel1.sh
+++ vowel1.sh
@@ -4,11 +4,13 @@
 #vowels
 
 read -p "Enter a character - " character
-
-if [[ "$character" == [aeiou] ]]
-then
+lower="aeiou"
+upper="AEIOU"
+echo
+if [[ "$character" == [$upper] || "$character" == [$lower] ]]
+then      
 	echo "$character is a vowel"
 else
-	echo "$character is not a vowel"
+	echo "$character not a vowel"
 fi
 
